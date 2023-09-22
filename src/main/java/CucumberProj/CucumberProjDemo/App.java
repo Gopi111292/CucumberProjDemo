0acba846d7ca98fdddd17f62ddb5a8bd251ab747
Gopi <gopiweb11@gmail.com>
2023-09-22 09:35:10 +0530

    first commit

---
+++ src/main/java/CucumberProj/CucumberProjDemo/App.java
@@ -0,0 +1,13 @@
+package CucumberProj.CucumberProjDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
